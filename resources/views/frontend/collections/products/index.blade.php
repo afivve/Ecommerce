0ee7f44cf47c52742327448356e9cc2427133026
afifdgr <afifdgr3@gmail.com>
2022-10-14 05:58:05 +0700

    #20 Convert display products in Livewire Component

--- resources/views/frontend/collections/products/index.blade.php
+++ resources/views/frontend/collections/products/index.blade.php
@@ -19,47 +19,9 @@
                 <div class="col-md-12">
                     <h4 class="mb-4">Our Products</h4>
                 </div>
-                @forelse ($products as $productItem)
-                    <div class="col-md-3">
-                        <div class="product-card">
-                            <div class="product-card-img">
-                                @if ($productItem->quantity > 0)
-                                    <label class="stock bg-success">In Stock</label>
-                                @else
-                                    <label class="stock bg-success">Out of Stock</label>
-                                @endif
 
-                                @if ($productItem->productImages->count() > 0)
-                                    <a
-                                        href="{{ url('/collections/' . $productItem->category->slug . '/' . $productItem->slug) }}">
-                                        {{ $productItem->name }}
-                                        <img src="{{ asset($productItem->productImages[0]->image) }}"
-                                            alt="{{ $productItem->name }}">
-                                    </a>
-                                @endif
-                            </div>
-                            <div class="product-card-body">
-                                <p class="product-brand">{{ $productItem->brand }}</p>
-                                <h5 class="product-name">
-                                    <a
-                                        href="{{ url('/collections/' . $productItem->category->slug . '/' . $productItem->slug) }}">
-                                        {{ $productItem->name }}
-                                    </a>
-                                </h5>
-                                <div>
-                                    <span class="selling-price">{{ $productItem->selling_price }}</span>
-                                    <span class="original-price">{{ $productItem->original_price }}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                @empty
-                    <div class="col-md-12">
-                        <div class="p-2">
-                            <h4>No Products Available for {{ $category->name }}</h4>
-                        </div>
-                    </div>
-                @endforelse
+                <livewire:frontend.product.index :products="$products" :category="$category" />
+
             </div>
         </div>
     </div>
