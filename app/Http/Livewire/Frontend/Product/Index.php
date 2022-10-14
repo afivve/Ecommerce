a9385c9e61b8e7a7e9d32176da4e802a2bf71cb0
afifdgr <afifdgr3@gmail.com>
2022-10-14 14:46:48 +0700

    #23 Filter products by PRICE

--- app/Http/Livewire/Frontend/Product/Index.php
+++ app/Http/Livewire/Frontend/Product/Index.php
@@ -7,10 +7,11 @@ use Livewire\Component;
 
 class Index extends Component
 {
-    public $products, $category, $brandInputs = [];
+    public $products, $category, $brandInputs = [], $priceInput;
 
     protected $queryString = [
         'brandInputs' => ['except' => '', 'as' => 'brand'],
+        'priceInput' => ['except' => '', 'as' => 'price'],
     ];
 
     public function mount($category)
@@ -22,6 +23,15 @@ class Index extends Component
         $this->products = Product::where('category_id', $this->category->id)
             ->when($this->brandInputs, function ($q) {
                 $q->whereIn('brand', $this->brandInputs);
+            })
+            ->when($this->priceInput, function ($q) {
+
+                $q->when($this->priceInput == 'high-to-low', function ($q2) {
+                    $q2->orderBy('selling_price', 'DESC');
+                })
+                    ->when($this->priceInput == 'low-to-high', function ($q2) {
+                        $q2->orderBy('selling_price', 'ASC');
+                    });
             })
             ->where('status', '0')
             ->get();
--- resources/views/livewire/frontend/product/index.blade.php
+++ resources/views/livewire/frontend/product/index.blade.php
@@ -16,6 +16,22 @@
                     </div>
                 </div>
             @endif
+
+            <div class="card mt-3">
+                <div class="card-header">
+                    <h4>Price</h4>
+                </div>
+                <div class="card-body">
+
+                    <label class="d-block">
+                        <input type="radio" name="priceSort" wire:model="priceInput" value="high-to-low"> High to Low
+                    </label>
+                    <label class="d-block">
+                        <input type="radio" name="priceSort" wire:model="priceInput" value="low-to-high"> Low to High
+                    </label>
+
+                </div>
+            </div>
         </div>
         <div class="col-md-9">
 
